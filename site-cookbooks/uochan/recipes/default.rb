e925ea733dbbc2d27dad2a918cf2a31dc81e4996
Masashi Iizuka <liquidz.uo@gmail.com>
2013-09-03 17:53:56 +0900

    update recipes

--- site-cookbooks/uochan/recipes/default.rb
+++ site-cookbooks/uochan/recipes/default.rb
@@ -14,21 +14,78 @@
     end
 end
 
+# private setting {{{
 git "/home/vagrant/.dotfiles" do
-    #repository "https://github.com/liquidz/dotfiles.git"
     repository "git://github.com/liquidz/dotfiles.git"
-    reference "master"
-    action :sync
+    reference  "master"
+    action     :sync
+    user       "vagrant"
+    group      "vagrant"
 end
-
-bash "setting dot files" do
-    cwd "/home/vagrant"
-    #not_if "ls .vimrc"
-
+directory "/home/vagrant/.backup" do
+    owner  "vagrant"
+    group  "vagrant"
+    mode   0755
+    action :create
+end
+bash "add symlinks from .dotfiles" do
+    cwd   "/home/vagrant"
+    user  "vagrant"
+    group "vagrant"
     code <<-EOC
-        ln -s .dotfiles/.gemrc     .
-        ln -s .dotfiles/.tmux.conf .
-        ln -s .dotfiles/.vimrc     .
-        ln -s .dotfiles/.zshrc     .
+        find .dotfiles -maxdepth 1 -type f | xargs -i ln -s {} .
     EOC
+    not_if 'ls /home/vagrant/.vimrc'
 end
+# }}}
+
+# neobundle {{{
+directory "/home/vagrant/.vim/bundle" do
+    owner     "vagrant"
+    group     "vagrant"
+    mode      0755
+    action    :create
+    recursive true
+end
+
+git "/home/vagrant/.vim/bundle/neobundle.vim" do
+    repository "git://github.com/Shougo/neobundle.vim.git"
+    reference  "master"
+    action     :sync
+    user       "vagrant"
+    group      "vagrant"
+end
+# }}}
+
+# java {{{
+%w{java-1.7.0-openjdk java-1.7.0-openjdk-devel}.each do |pkg|
+    package pkg do
+        action :install
+    end
+end
+# }}}
+
+# leiningen {{{
+directory "#{node['leiningen']['home']}/bin" do
+    owner     node['leiningen']['user']
+    group     node['leiningen']['group']
+    mode      0755
+    action    :create
+end
+
+bash "self-install leiningen" do
+    cwd   "#{node['leiningen']['home']}/bin"
+    user  node['leiningen']['user']
+    group node['leiningen']['group']
+    environment "HOME"        => node['leiningen']['home'],
+                "HTTP_CLIENT" => node['leiningen']['http_client'],
+                "HTTPS_PROXY" => node['leiningen']['https_proxy']
+    code <<-EOC
+        wget #{node['leiningen']['script']}
+        chmod +x /home/vagrant/bin/lein
+        #{node['leiningen']['home']}/bin/lein self-install
+    EOC
+    not_if "ls #{node['leiningen']['home']}/bin/lein"
+end
+# }}}
+
