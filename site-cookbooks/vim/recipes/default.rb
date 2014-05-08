2af85e444c500105140739016b01218bca47b405
uochan <liquidz.uo@gmail.com>
2014-05-08 17:57:20 +0900

    update

--- site-cookbooks/vim/recipes/default.rb
+++ site-cookbooks/vim/recipes/default.rb
@@ -8,5 +8,42 @@
 #
 
 package 'vim' do
-    action :install
+    action :remove
 end
+
+%w{mercurial lua lua-devel}.each do |pkg_name|
+    package pkg_name do
+        action :install
+    end
+end
+
+git "/usr/local/src/luajit" do
+    repository "http://luajit.org/git/luajit-2.0.git"
+    reference "master"
+    action :sync
+end
+
+bash "build luajit" do
+    cwd "/usr/local/src/luajit"
+    code <<-EOT
+make
+make install
+EOT
+end
+
+bash "clone vim source" do
+    cwd "/usr/local/src"
+    code <<-EOT
+hg clone https://vim.googlecode.com/hg/ vim
+EOT
+end
+
+bash "build vim from source" do
+    cwd "/usr/local/src/vim"
+    code <<-EOT
+./configure --prefix=/usr/local --with-features=huge --enable-multibyte --enable-luainterp --with-luajit --enable-fail-if-missing
+make
+make install
+EOT
+end
+
